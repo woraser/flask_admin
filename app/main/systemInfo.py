f97f7f62ed485a74303554ac829c599710ea9fd0
charles.chen <809728954@qq.com>
2017-06-02 10:13:41 +0800

    add dashboard systemInfo
    get systemInfo

---
+++ app/main/systemInfo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'chen hui'
+
+
+def getMemoryInfo():
+    pass
+
+
+def getHDInfo():
+    pass
+
+
+def getCpuInfo():
+    pass
--- config.py
+++ config.py
@@ -27,8 +27,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
 
 
 config = {
