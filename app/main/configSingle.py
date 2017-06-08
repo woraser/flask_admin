9676b6f09bede7965796cb1f288ba1e7e118445f
charles.chen <809728954@qq.com>
2017-06-08 16:59:58 +0800

    add ini

---
+++ app/main/configSingle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import ConfigParser
+
+def singleton(cls, *args, **kw):
+    instances = {}
+    def _singleton():
+        if cls not in instances:
+            instances[cls] = cls(*args, **kw)
+        return instances[cls]
+    return _singleton
+
+
+
+@singleton
+class configObj(object):
+
+    config_obj = None
+    def __init__(self):
+        if self.config_obj is None:
+            self.config_obj = ConfigParser.ConfigParser()
+            with open('../../config.ini') as fp:
+                self.config_obj.readfp(fp)
+        pass
+
+
+
+if __name__ == '__main__':
+    ss = configObj()
+    ss2 = configObj()
+    ss3 = configObj()
+    ins1 = ss.config_obj
+    ins2 = ss2.config_obj
+    ins3 = ss3.config_obj
+    print id(ins1)
+    print id(ins2)
+    print id(ins3)
