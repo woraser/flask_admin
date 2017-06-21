9bb210f12d0c22c830488456595ef12645b91059
charles.chen <809728954@qq.com>
2017-06-21 16:36:45 +0800

    add

--- app/main/systemInfo.py
+++ app/main/systemInfo.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # tip:http://homeway.me/2015/04/29/openwrt-develop-base-util/
 # Title:系统级别参数
+from __future__ import division
 from app.main import configSingle
 from app.common.dbFactory import getTablePageByCls
 import os
@@ -43,6 +44,29 @@ def getRunTime():
     return res[0]
     pass
 
+# 获取硬盘总量
+def getHardDiskTotal():
+    with os.popen("df | awk '{print $2}'") as ps:
+        res = ps.read().strip().split("\n")
+    int_list = [(int(i)) for i in res[1:]]
+    return sum(int_list)
+    pass
+
+# 获取硬盘使用量
+def getHardDiskUsed():
+    with os.popen("df | awk '{print $3}'") as ps:
+        res = ps.read().strip().split("\n")
+    int_list = [(int(i)) for i in res[1:]]
+    return sum(int_list)
+    pass
+
+# 获取硬盘的使用率
+def getHardDiskUseage():
+    df_total = getHardDiskTotal()
+    df_used = getHardDiskUsed()
+    return round(df_used/df_total, 4)*100
+
+
 # build dict of dashboard for index
 # 组装首页面板数据
 def getDashboard():
@@ -59,7 +83,7 @@ def getDashboard():
     pass
 
 def getSystemHistory(limit=500):
-    orderBy = ('i', 'desc')
+    orderBy = ('id', 'desc')
     cursor = getTablePageByCls("SystemInfo", limit=limit, order=orderBy)
     return cursor
     pass
